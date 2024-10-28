24938e2d7f3b3e501c014e444bc0b6f19bd341f5
Sinnus <sinnus@qq.com>
2024-10-28 20:48:26 +0800

    update huawei model

--- lstm/huawei/right_lstm_kill.py
+++ lstm/huawei/right_lstm_kill.py
@@ -19,10 +19,10 @@ window_size = 130
 feature_size = 6
 num_classes = 3
 
-hidden_size = 128
+hidden_size = 256
 output_size = num_classes
 input_shape = (window_size, feature_size)
-batch_size = 32  # (128,50: 14,16), (32,50: 17,14),
+batch_size = 64  # (128,50: 14,16), (32,50: 17,14),
 epochs = 50  # (128,20: 20, 8) (32,20: 15, 16)
 ##### 这里64，128， 50 最好。
 ##### 这里64，32， 50 最好。
